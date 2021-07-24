020dd15049dcf0576192ac5ae2ab6f9393b6dd55
ds3Tomas <ingenieriacreativads3@gmail.com>
2021-07-24 11:01:18 -0300

    change msg

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ var express = require('express');
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello world, i am ds3tomas');
+ res.send('hello world, i am ');
 });
 //listen to port 3000 by default chinga
 let port = process.env.PORT || 8081
-console.log('hello world')
-console.log('hello world')
 console.log('hello world')
 console.log('listen in port ' + port) // nuevo port
 app.listen(process.env.PORT || 8081);
